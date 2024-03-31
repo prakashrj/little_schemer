7e649aa8238bead093421f1294103d053af6f4d9
sahillakhmani <111144794+sahillakhmani@users.noreply.github.com>
2024-04-01 00:25:16 +0530

    Update 7.7.rkt
    Added the condition for handling 'NOT

--- Sahil/7.7.rkt
+++ Sahil/7.7.rkt
@@ -21,11 +21,14 @@
             ((null? lexp) #t)
             ((atom? lexp) (search lexp los))
             ((or (equal? (car lexp) 'AND) (equal? (car lexp) 'OR)) (and (covered? (car (cdr lexp)) los) (covered? (car (cdr (cdr lexp))) los)))
+            ((equal? (car lexp) 'NOT) (covered? (car (cdr lexp)) los))
         )
     )
 )
 
 (define lexp1 '(AND (OR x y) z))
+(define lexp2 '(NOT (OR x y)))
+
 (define los '(x y z))
 (define los1 '(x y))
 (define los2 '(x z))
@@ -33,6 +36,7 @@
 (define los4 '(a b y x z c))
 
 (covered? lexp1 los)
+(covered? lexp2 los)
 (covered? lexp1 los1)
 (covered? lexp1 los2)
 (covered? lexp1 los4)
