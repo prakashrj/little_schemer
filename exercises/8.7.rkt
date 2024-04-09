35a0d17b5f6bfed9a1523f44ee2e72a0d4e2202e
prakashrj <prakashrj@yahoo.com>
2024-04-09 14:30:17 -0700

    solution for problem

---
+++ exercises/8.7.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(define Rin
+  (lambda (set a)
+    (cond
+      ((null? set) '())
+      (else (cons (cons a (cons (car set) '())) (Rin (cdr set) a))))))
+     
+
+(define r1 '((a b) (a a) (b b)))
+(define r2 '((c c)))
+(define r3 '((a c) (b c)))
+(define r4 '((a b) (b a)))
+(define f1 '((a 1) (b 2) (c 2) (d 1)))
+(define f2 '())
+(define f3 '((a 2) (b 1)))
+(define f4 '((1 $) (3 *)))
+(define d1 '(a b))
+(define d2 '(c d))
+(define x 'a)
+
+(Rin d1 x)
+(Rin d2 x)
+(Rin f2 x)
