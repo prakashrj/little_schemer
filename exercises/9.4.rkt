33459423933c75ec58a03787bf617ef2bc9ede85
prakashrj <prakashrj@yahoo.com>
2024-06-19 08:44:37 -0700

    solution for problem

--- exercises/9.4.rkt
+++ exercises/9.4.rkt
@@ -12,9 +12,34 @@
 	  (lambda (l)
 	    (cond
 	      ((equal? a (car l)) (cdr l))
-	      (else (cons (car l) (f (cdr l) a))))))) a l)))
+	      (else (cons (car l) (f (cdr l)))))))) l)))
+
+(define insertR
+  (lambda (new old lat)
+    ((Y (lambda (f)
+	  (lambda (lat)
+	    (cond
+	      ((null? lat) '())
+	      ((equal? (car lat) old) (cons old (cons new (cdr lat))))
+	      (else (cons (car lat) (f (cdr lat)))))))) lat)))
+
+(define subst2
+  (lambda (new o1 o2 lat)
+    ((Y (lambda (f)
+	  (lambda (lat)
+	    (cond
+	      ((null? lat) '())
+	      ((or (eq? (car lat) o1) (eq? (car lat) o2)) (cons new (cdr lat)))
+	      (else (cons (car lat) (f (cdr lat)))))))) lat)))
+
+
 
 (define lat '(wad da hail))
 (define a 'da)
+(define new 'sigma)
+(define old 'da)
+(define b 'wad)
 
 (rember a lat)
+(insertR new old lat)
+(subst2 new a b lat)
